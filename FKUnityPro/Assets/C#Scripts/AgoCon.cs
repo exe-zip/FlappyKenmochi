f6736af21fb5101db92cbaa2eb0dec908c42e75c
exe-zip <harisamaruki1009@gmail.com>
2022-01-01 22:28:40 +0900

    角度制御コード追加、それぞれの処理ごとに関数で管理

--- FKUnityPro/Assets/C#Scripts/AgoCon.cs
+++ FKUnityPro/Assets/C#Scripts/AgoCon.cs
@@ -4,25 +4,42 @@ using UnityEngine;
 
 public class AgoCon : MonoBehaviour
 {
-    float move;
+    float move, rotate;
+    bool damage;
 
     void Start()
     {
         move = 0.12f;
+        damage = false;
     }
 
     void Update()
     {
+        Jump();
+        Fall();
+        Debug.Log(damage);
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        damage = true;
+    }
+
+    void Jump()
+    {
         if (Input.GetKeyDown(KeyCode.Space))
         {
             move = 0.12f;
+            rotate = 0f;
+            this.transform.eulerAngles = new Vector3(0f, 0f, 20f);
         }
-        this.transform.position += new Vector3(0f, move, 0f);
-        move -= 0.0058f;
     }
 
-    private void OnTriggerStay2D(Collider2D other)
+    void Fall()
     {
-        Debug.Log("a");
+        this.transform.position += new Vector3(0f, move, 0f);
+        this.transform.eulerAngles += new Vector3(0f, 0f, rotate);
+        move -= 0.0058f;
+        rotate -= 0.04f;
     }
 }
